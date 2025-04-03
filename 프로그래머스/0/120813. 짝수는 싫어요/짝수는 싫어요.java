9c0e96137e869864cdf88cfbcb5b32e79ca43101
ohgongjuu0406 <ohgongjuu0406@gmail.com>
2025-04-03 21:50:56 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.05 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i = 0; i<=n; i++){
+            if(i % 2 != 0)
+                answer.add(i);
+        }
+        return answer;
+    }
+}
