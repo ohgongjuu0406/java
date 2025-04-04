80c301f463d63e956184ad95f40a142d3701f2fa
ohgongjuu0406 <ohgongjuu0406@gmail.com>
2025-04-04 12:31:38 +0900

    [level 0] Title: 369게임, Time: 15.18 ms, Memory: 85.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        String s = order + "";
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '3' || s.charAt(i) == '6' || s.charAt(i) == '9')
+                answer +=1;
+        }
+        return answer;
+    }
+}
